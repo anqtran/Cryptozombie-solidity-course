2555e1b233bdaa498ba9bf5fc60b56d2dd400901
An Q Tran <anqtran@gatech.edu>
2018-01-23 21:26:48 +0800

    Added chapter 3

--- contract/zombiefactory.sol
+++ contract/zombiefactory.sol
@@ -1,15 +1,21 @@
 pragma solidity ^0.4.19;
 
-contract ZombieFactory {
+import "./ownable.sol";
+
+contract ZombieFactory is Ownable {
 
     event NewZombie(uint zombieId, string name, uint dna);
 
+    // uint, default as uint256
     uint dnaDigits = 16;
+    // 10 ^ dnaDigits
     uint dnaModulus = 10 ** dnaDigits;
 
     struct Zombie {
         string name;
         uint dna;
+        uint32 level;
+        uint32 readyTime;
     }
 
     Zombie[] public zombies;
@@ -25,6 +31,9 @@ contract ZombieFactory {
     }
 
     function _generateRandomDna(string _str) private view returns (uint) {
+        // keccak256 built in, which is a version of SHA3. 
+        // A hash function basically maps an input string into a random 256-bit hexidecimal number. 
+        // 64 characters
         uint rand = uint(keccak256(_str));
         return rand % dnaModulus;
     }
@@ -32,6 +41,7 @@ contract ZombieFactory {
     function createRandomZombie(string _name) public {
         require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
