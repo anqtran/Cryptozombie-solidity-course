e19201d2f9cc8ce19b17530e8bf18b5584d22438
An Q Tran <anqtran@gatech.edu>
2018-02-17 06:17:19 +0800

    Added chapter 4

--- contract/zombiefactory.sol
+++ contract/zombiefactory.sol
@@ -6,16 +6,17 @@ contract ZombieFactory is Ownable {
 
     event NewZombie(uint zombieId, string name, uint dna);
 
-    // uint, default as uint256
     uint dnaDigits = 16;
-    // 10 ^ dnaDigits
     uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
 
     struct Zombie {
-        string name;
-        uint dna;
-        uint32 level;
-        uint32 readyTime;
+      string name;
+      uint dna;
+      uint32 level;
+      uint32 readyTime;
+      uint16 winCount;
+      uint16 lossCount;
     }
 
     Zombie[] public zombies;
@@ -24,16 +25,13 @@ contract ZombieFactory is Ownable {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string _str) private view returns (uint) {
-        // keccak256 built in, which is a version of SHA3. 
-        // A hash function basically maps an input string into a random 256-bit hexidecimal number. 
-        // 64 characters
         uint rand = uint(keccak256(_str));
         return rand % dnaModulus;
     }
